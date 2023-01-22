1dcd43adb460d68d0849f2c0dfa0c653075dd45e
Mark Coats <alexco1234@gmail.com>
2023-01-21 20:12:32 -0600

    added loops :#

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, message){
+    let arr = [];
+    //Thank you, Guadalupe, for the wonderful surprise gift!
+    for (let cnt = 0; cnt <= names.length-1; cnt++){
+        arr.push(`Thank you, ${names[cnt]}, for the wonderful ${message} gift!`);
+    }
+    return arr;
+}
+
+function countDown(num){
+    for (num; num >= 0; num--){
+        console.log(num);
+    }
+}
+
+//writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+countDown(100);
